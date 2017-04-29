ce904499b72f2142d50ee6faf6cef5b7cd18ecd3
桐铭 梁 <werltm@hotmail.com>
2017-04-29 09:59:16 +0800

    删除冗余指令文件。

---
+++ src/YoYo.Cms.Web/App/Admin/views/layout/header/headerCtrl.js
@@ -0,0 +1,24 @@
+﻿(function() {
+
+    'use strict';
+    yoyocmsModule.controller('app.layout.header',header);
+
+    function header($scope, appSession) {
+        var vm = this;
+        vm.languages = abp.localization.languages;
+        vm.currentLanguage = abp.localization.currentLanguage;
+
+        vm.changeLanguage = function (languageName) {
+            console.log($scope.languages);
+
+            location.href = abp.appPath + 'AbpLocalization/ChangeCulture?cultureName=' + languageName + '&returnUrl=' + window.location.pathname + window.location.hash;
+        };
+
+
+        vm.con = function () {
+            console.log("d");
+        }
+
+    }
+
+})();
